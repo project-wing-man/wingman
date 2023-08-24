b0f5ab3e117f928776569e3678c31862be4265df
Matt Mattox <bizofmatt@gmail.com>
2023-08-23 18:08:17 -0700

    test

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+const React = require('react');
+const createRoot = require('react-dom/client')
+
+
+const container = document.getElementById('app-root');
+const root = createRoot(container)
+root.render(<h1>poopy</h1>)
+
+
+
