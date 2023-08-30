a68aca8ea1c432db754740cf460186acb9f4b027
Matt Mattox <bizofmatt@gmail.com>
2023-08-29 18:01:47 -0700

    setting up flights controller

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
+const apiRouter = './routes/api';
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
+
+app.use('/api', apiRouter);
+
+//global error handler
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    status: 500,
+    message: err.message
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+export default app;
